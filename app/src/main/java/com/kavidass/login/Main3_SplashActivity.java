2eb446d305f498781906630c5a1458c49aedfe1f
kavidass <kavidass89@gmail.com>
2018-11-27 20:40:02 +0530

    second commit

---
+++ app/src/main/java/com/kavidass/login/Main3_SplashActivity.java
@@ -0,0 +1,13 @@
+package com.kavidass.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main3_SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3__splash);
+    }
+}
